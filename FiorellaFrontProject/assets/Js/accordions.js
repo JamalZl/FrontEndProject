799fb1f4dd26597c991f6b10cbf6e1066e32a677
Camal <90691266+JamalZl@users.noreply.github.com>
2021-12-08 17:57:17 +0400

    Add files via upload

--- FiorellaFrontProject/assets/Js/accordions.js
+++ FiorellaFrontProject/assets/Js/accordions.js
@@ -1,17 +1,17 @@
-// $(document).ready(function () {
-//     $(".icon-title").click(function(){
-//         let next=$(this).next();
-//         $(".accordionText").not(next).slideUp();
-//         $(this).next().slideToggle();
-//     })
-// })
-$(document).ready(function() {
-    $(".title-icon").click(function() {
-        let next = $(this).next();
-        let icon = $(this).children(".icon");
-        $(".article").not(next).slideUp();
+$(document).ready(function () {
+    $(".icon-title").click(function(){
+        let next=$(this).next();
+        $(".accordionText").not(next).slideUp();
         $(this).next().slideToggle();
-        $(".icon").not(icon).removeClass("active")
-        icon.toggleClass("active")
     })
 })
+// $(document).ready(function() {
+//     $(".title-icon").click(function() {
+//         let next = $(this).next();
+//         let icon = $(this).children(".icon");
+//         $(".article").not(next).slideUp();
+//         $(this).next().slideToggle();
+//         $(".icon").not(icon).removeClass("active")
+//         icon.toggleClass("active")
+//     })
+// })
--- FiorellaFrontProject/assets/Js/home.js
+++ FiorellaFrontProject/assets/Js/home.js
@@ -41,65 +41,14 @@ $(document).ready(function () {
         }
     })
 
-    // $(".menu-icon i").click(function () {
-    //     $(".menu").css("display", "block")
-    //     $(".menu").addClass("animate__fadeInRight")
-    //     $(".menu").removeClass("animate__fadeOutRight")
+   let addToCards=document.querySelectorAll("#productList .products .product .text")
+   let products=document.querySelector(".basketProducts")
 
-    // })
-    // $(".closeX i").click(function () {
-    //     $(".menu").removeClass("animate__fadeInRight")
-    //     $(".menu").addClass("animate__fadeOutRight")
-    // })
-    // $(".menuHomeLi a").click(function () {
-    //     $(".menuHome").slideToggle();
-    //     $(".menuHomeLi i").toggleClass("myRotate")
-    // })
-
-    // $(".menuPagesLi a").click(function () {
-    //     $(".menuPages").slideToggle();
-    //     $(".menuPagesLi i").toggleClass("myRotate");
-    // })
-    // $(".menuShopLi a").click(function () {
-    //     $(".menuShop").slideToggle();
-    //     $(".menuShopLi i").toggleClass("myRotate");
-    // })
-    // $(".pth4").click(function () {
-    //     $(".pt").slideToggle();
-    //     $(".pth4 i").toggleClass("myRotate");
-    // })
-    // $(".sph4").click(function () {
-    //     $(".sp").slideToggle();
-    //     $(".sph4 i").toggleClass("myRotate");
-    // })
-    // $(".sth4").click(function () {
-    //     $(".st").slideToggle();
-    //     $(".sth4 i").toggleClass("myRotate");
-    // })
-    // $(".slh4").click(function () {
-    //     $(".sl").slideToggle();
-    //     $(".slh4 i").toggleClass("myRotate");
-    // })
-
-    // $(".menuPortfolioLi a").click(function () {
-    //     $(".menuPortfolio").slideToggle();
-    //     $(".menuPortfolioLi i").toggleClass("myRotate");
-    // })
-    // $(".menuPortfolioLayoutsLi a").click(function () {
-    //     $(".menuPortfolio-inner-layouts").slideToggle();
-    //     $(".menuPortfolioLayoutsLi i").toggleClass("myRotate");
-    // })
-    // $(".menuPortfolioSingleLi a").click(function () {
-    //     $(".menuPortfolio-inner-single").slideToggle();
-    //     $(".menuPortfolioSingleLi i").toggleClass("myRotate");
-    // })
-    // $(".menuBlogLi a").click(function () {
-    //     $(".menuBlog").slideToggle();
-    //     $(".menuBlogLi i").toggleClass("myRotate");
-    // })
-    // $(".menuBlogPostLi a").click(function () {
-    //     $(".menuBlog-inner-post").slideToggle();
-    //     $(".menuBlogPostLi i").toggleClass("myRotate");
-    // })
-
+   addToCards.forEach(addToCard =>{
+       addToCard.onclick=function(e){
+           let Id=this.parentNode.parentNode.parentNode.getAttribute("data-id")
+           let img=this.parentNode.parentNode.parentNode.children[0].children[0].getAttribute("src")
+           console.log(img);
+       }
+   })
 })
